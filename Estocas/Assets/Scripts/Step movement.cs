4c9dac792c4539dc2f1d017555c1ec9a02d24653
Teo <83099938+Teo-Diaz@users.noreply.github.com>
2024-10-23 00:26:48 -0500

    Movimiento funcional
    Falta en diagonal

--- Estocas/Assets/Scripts/Step movement.cs
+++ Estocas/Assets/Scripts/Step movement.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Stepmovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            transform.Translate(0, 1.1f, 0);
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+            transform.Translate(0, -1.1f, 0);
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            transform.Translate(-1.1f, 0, 0);
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            transform.Translate(1.1f, 0, 0);
     }
 }
